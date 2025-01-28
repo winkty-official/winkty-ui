5fec25dae587b46dbd14ca9e7ea1acf5c9d3a76a
Avinash <61863628+Avinash-sord12k@users.noreply.github.com>
2025-01-29 01:49:07 +0530

    fix: build

--- src/app/components/(basic)/input/data/package.ts
+++ src/app/components/(basic)/input/data/package.ts
@@ -2,7 +2,7 @@ import manual from "../../../../../../public/registry/simple-input.json";
 
 export const packageInfo = {
   cli: {
-    command: "https://your-domain.com/registry/simple-input.json",
+    command: `${process.env.BASE_URI}/registry/simple-input.json`,
   },
   manual,
 };
--- src/app/components/(basic)/radio/data/package.ts
+++ src/app/components/(basic)/radio/data/package.ts
@@ -2,7 +2,7 @@ import manual from "../../../../../../public/registry/area-radio-group.json";
 
 export const packageInfo = {
   cli: {
-    command: "https://your-domain.com/registry/area-radio-group.json",
+    command: `${process.env.BASE_URI}/registry/area-radio-group.json`,
   },
   manual,
 };
